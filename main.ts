85946c48cf4f1b59e9186b90e7d875a5a5d7db53
gchamb <gchamb.dev@gmail.com>
2025-07-18 09:03:32 -0500

    umm yeah umm yeah

--- main.ts
+++ main.ts
@@ -26,56 +26,56 @@ async function backup() {
   const ACCESS_KEY = process.env.MY_AWS_ACCESS_KEY_ID;
   const SECRET_ACCESS_KEY = process.env.MY_AWS_SECRET_ACCESS_KEY;
 
-  if (
-    MYSQL_HOST === undefined ||
-    MYSQL_USER === undefined ||
-    MYSQL_PASS === undefined ||
-    MYSQL_PORT === undefined ||
-    MYSQL_DATABASE === undefined ||
-    ENV === undefined ||
-    BUCKET_NAME === undefined ||
-    ACCESS_KEY === undefined ||
-    SECRET_ACCESS_KEY === undefined
-  ) {
-    throw new Error("Not all environment variables are set. Check config.");
+  try {
+    if (
+      MYSQL_HOST === undefined ||
+      MYSQL_USER === undefined ||
+      MYSQL_PASS === undefined ||
+      MYSQL_PORT === undefined ||
+      MYSQL_DATABASE === undefined ||
+      ENV === undefined ||
+      BUCKET_NAME === undefined ||
+      ACCESS_KEY === undefined ||
+      SECRET_ACCESS_KEY === undefined
+    ) {
+      throw new Error("Not all environment variables are set. Check config.");
+    }
+
+    const client = new S3Client({
+      region: "us-east-2",
+      credentials: {
+        accessKeyId: ACCESS_KEY,
+        secretAccessKey: SECRET_ACCESS_KEY,
+      },
+    });
+
+    const fileName = `${now}-backup.sql`;
+    const command = `mysqldump -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASS} --port ${MYSQL_PORT} ${MYSQL_DATABASE} > ${fileName}`;
+
+    console.log("Executing Backup..");
+
+    execSync(command);
+
+    if (!fileExists(fileName)) {
+      throw new Error("Backup file doesn't exist.");
+    }
+
+    console.log("Dump operation successfully completed.");
+
+    const putCommand = new PutObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: `${ENV}/mysql/${fileName}`,
+      Body: await fs.readFile(fileName),
+    });
+
+    console.log("Uploading to s3...");
+
+    await client.send(putCommand);
+
+    console.log("Backup is complete.");
+  } catch (err) {
+    console.error("Error occurred while backing up database", err);
   }
-
-  const client = new S3Client({
-    region: "us-east-2",
-    credentials: {
-      accessKeyId: ACCESS_KEY,
-      secretAccessKey: SECRET_ACCESS_KEY,
-    },
-  });
-
-  const fileName = `${now}-backup.sql`;
-  const command = `mysqldump -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASS} --port ${MYSQL_PORT} ${MYSQL_DATABASE} > ${fileName}`;
-
-  console.log("Executing Backup..");
-
-  execSync(command);
-
-  if (!fileExists(fileName)) {
-    throw new Error("Backup file doesn't exist.");
-  }
-
-  console.log("Dump operation successfully completed.");
-
-  const putCommand = new PutObjectCommand({
-    Bucket: BUCKET_NAME,
-    Key: `${ENV}/mysql/${fileName}`,
-    Body: await fs.readFile(fileName),
-  });
-
-  console.log("Uploading to s3...");
-
-  await client.send(putCommand);
-
-  console.log("Backup is complete.");
 }
 
-try {
-  backup();
-} catch (err) {
-  console.error("Error occurred while backing up database", err);
-}
+backup();
